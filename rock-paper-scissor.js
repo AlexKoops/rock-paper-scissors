ace8ef9fbaa27f751c17fa91ae10b62fce24e9a3
AlexKoops <125556152+AlexKoops@users.noreply.github.com>
2024-02-28 22:30:09 +0100

    feat: Add gameplay functions to Rock, Paper, Scissors
    - Implemented a function to play a single round using player and computer selections. The outcome (win/lose/draw) is logged to the console.
    - Added a function to play the game five times in a row, keeping track of and displaying the score after each round.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -3,15 +3,47 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     let choice = '';
     if (randomNumber == 0) {
-        choice = 'Rock';
+        choice = 'rock';
     }
     else if (randomNumber == 1) {
-        choice = 'Paper';
+        choice = 'paper';
     }
     else {
-        choice = 'Scissors';
+        choice = 'scissors';
     }
     return choice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    if(playerChoice === "rock" && computerSelection === "paper") {
+        console.log("You lose! Paper beats Rock");
+    }
+    else if(playerChoice === "paper" && computerSelection === "scissors") {
+        console.log("You lose! Scissors beats Paper");
+    }
+    else if(playerChoice === "scissors" && computerSelection === "rock") {
+        console.log("You lose! Rock beats Scissors");
+    }
+    else if(playerChoice === "paper" && computerSelection === "rock") {
+        console.log("You win! Paper beats Rock");
+    }
+    else if(playerChoice === "scissors" && computerSelection === "paper") {
+        console.log("You win! Scissors beats paper");
+    }
+    else if(playerChoice === "rock" && computerSelection === "scissors") {
+        console.log("You win! Rock beats Scissors")
+    }
+    else {
+        console.log("It is a draw!");
+    }
+}
+
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Choose rock, paper of scissors");
+        playRound(playerChoice, getComputerChoice());
+    }
+}
+
+playGame();
